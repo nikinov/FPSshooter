86e1d315c58c3552884f77c01bccd42624b3b071
nikinov <novellenicholas@gmail.com>
2020-08-14 12:55:27 +0200

    floating bug fix

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(ConfigurableJoint))]
 public class PlayerMotor : MonoBehaviour
 {
     [SerializeField] private Camera cam;
@@ -14,12 +15,18 @@ public class PlayerMotor : MonoBehaviour
     private Vector3 thrusterForse = Vector3.zero;
 
     [SerializeField] private float cameraRotationLimit = 85;
+    [SerializeField] private ConfigurableJoint joint;
+
+    public bool grounded;
+    private Vector3 posCur;
+    private Quaternion rotCur;
 
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        joint = GetComponent<ConfigurableJoint>();
     }
 
     // get movement vector
@@ -58,6 +65,23 @@ public class PlayerMotor : MonoBehaviour
         if (velocity != Vector3.zero)
         {
             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+            //declare a new Ray. It will start at this object's position and it's direction will be straight down from the object (in local space, that is)
+            Ray ray = new Ray(transform.position, -transform.up);
+            //decalre a RaycastHit. This is neccessary so it can get "filled" with information when casting the ray below.
+            RaycastHit hit;
+            //cast the ray. Note the "out hit" which makes the Raycast "fill" the hit variable with information. The maximum distance the ray will go is 1.5
+            if (Physics.Raycast(ray, out hit) == true)
+            {
+                //draw a Debug Line so we can see the ray in the scene view. Good to check if it actually does what we want. Make sure that it uses the same values as the actual Raycast. In this case, it starts at the same position, but only goes up to the point that we hit.
+                Debug.DrawLine(transform.position, hit.point, Color.green);
+                if (joint.connectedAnchor.y != hit.point.y + 1.5f)
+                {
+                    joint.connectedAnchor = new Vector3(0, hit.point.y + 1f, 0);
+                    Debug.Log(hit.point.y + 1);
+                }
+            }
+
+
         }
 
         if (thrusterForse != Vector3.zero)
