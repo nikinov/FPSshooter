35e77316774bc309be022cd67eb5025ca78f6e02
zaher <zaharshibakov666@gmail.com>
2020-08-19 18:04:59 +0300

    Transform

---
+++ Assets/zahar/Scripts/BackTransform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackTransform : MonoBehaviour
+{
+    public GameObject ex;
+    public GameObject transport;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Instantiate(ex, transform.position, transform.rotation);
+            Invoke("Transform", 0.1f);
+        }
+    }
+    void Transform()
+    {
+        transport.gameObject.SetActive(false);
+        player.gameObject.SetActive(true);
+    }
+}
